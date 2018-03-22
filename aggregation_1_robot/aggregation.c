0b21cba80dd5d6d6876ff1e88fc02aa2019b6b27
Thomas Taylor <thomas.taylor@etu.upmc.fr>
2018-03-22 18:39:30 +0100

    aggregation+

--- aggregation_1_robot/aggregation.c
+++ aggregation_1_robot/aggregation.c
@@ -2,26 +2,40 @@
 #define DEBUG
 #include <debug.h>
 #include <stdio.h>
-#include "main.h"
+#include "aggregation.h"
+
+#define SECONDE 32
+#define DIST_TO_AGGREGATE 50
 
 data_t data;
 data_t *mydata = &data;
 
+message_t message;
+
 void setup() {
-// put your setup code here, to be run only once
-//message.type = NORMAL;
-//message.crc = message_crc(&message);
-mydata->new_message = 0;
-mydata->last_dist_update = -1;
-mydata->toAggregate.dist = -1;
-mydata->state = SEARCHING;
-//mydata->curr_motion = STOP;
-set_motion(STOP);
-//mydata->toAggregate = NULL;
+	// put your setup code here, to be run only once
+	mydata->message.type = NORMAL;
+	mydata->message.data[0] = kilo_uid;
+	mydata->message.crc = message_crc(&mydata->message);
+
+	message.type = NORMAL;
+	message.data[0] = kilo_uid;
+	message.crc = message_crc(&message);
+
+	mydata->new_message = 0;
+	mydata->last_dist_update = -1;
+	mydata->toAggregate.dist = -1;
+	mydata->state = SEARCHING;
+	mydata->broadcast = 0;
+	//enable_tx = 0;
+	mydata->message_sent = 0;
+	//mydata->curr_motion = STOP;
+	set_motion(STOP);
+	//mydata->toAggregate = NULL;
 }
 
 void aggregation() {
-	if (kilo_ticks > mydata->last_motion_update + 32){
+	if (kilo_ticks > mydata->last_motion_update + SECONDE){
 		mydata->last_motion_update = kilo_ticks;
 
 		switch(mydata->state){
@@ -40,15 +54,14 @@ void aggregation() {
 	set_motion(mydata->curr_motion);
 	//printf("MOTION : %d\n",mydata->curr_motion);
 	//("DISTANCE.toAggregate : %d\n",mydata->toAggregate.dist);
-	printf("DISTANCE.last_dist_update : %d\n", mydata->last_dist_update);
+	//printf("DISTANCE.last_dist_update : %d\n", mydata->last_dist_update);
 }
 
 void searching(){
 	set_color(RGB(1,0,0));
 	mydata->last_motion_update = kilo_ticks;
-
+	printf("NEW MESSAGE : %d\n", mydata->new_message);
 	if(found_to_aggregate()){
-		mydata->new_message = 0;
 		mydata->state = CONVERGING;
 		//mydata->curr_motion = STRAIGHT;
 		set_motion(STRAIGHT);
@@ -62,6 +75,7 @@ void searching(){
 uint8_t found_to_aggregate(){
 	uint8_t found = 0;
 	if(mydata->new_message == 1){
+		mydata->new_message = 0;
 		mydata->toAggregate.timestamp = kilo_ticks;
 		found = 1;
 	}
@@ -75,7 +89,7 @@ void converging(){
 		//mydata->curr_motion = STOP;
 		set_motion(STOP);
 	} else {
-		if(mydata->toAggregate.dist > mydata->last_dist_update){
+		if(mydata->toAggregate.dist < mydata->last_dist_update){
 			printf("IN TEST");
 			switch (mydata->curr_motion) {
 				case RIGHT:
@@ -92,20 +106,43 @@ void converging(){
 					break;
 			}
 		}
+		if(!found_to_aggregate()){
+			mydata->state = SEARCHING;
+		}
+
 		mydata->last_dist_update = mydata->toAggregate.dist;
 	}
 
 }
 
 void sleeping(){
-	set_color(RGB(0,0,1));
+	//set_color(RGB(0,0,1));
 	set_motion(STOP);
+	mydata->broadcast = 1;
+	//enable_tx = 1;
+	if(!found_to_aggregate()){
+		mydata->state = SEARCHING;
+		mydata->broadcast = 0;
+		//enable_tx = 0;
+		mydata->new_message = 0;
+	}
+
+	if (mydata->message_sent == 1)
+	{
+			// Reset the flag so the LED is only blinked once per message.
+			mydata->message_sent = 0;
+
+			set_color(RGB(1, 0, 1));
+			delay(100);
+			set_color(RGB(0, 0, 0));
+	}
+	printf("broadcast : %d\n", mydata->broadcast);
 }
 
 uint8_t is_too_close(){
 	uint8_t stop = 0;
 	printf("dist dans is_too_close %d\n", mydata->toAggregate.dist);
-	if(mydata->toAggregate.dist <= 40){
+	if(mydata->toAggregate.dist <= DIST_TO_AGGREGATE){
 
 		stop = 1;
 	}
@@ -165,17 +202,39 @@ void set_random_turning_direction(){
 	}
 }
 
+int lost_aggregate(){
+	uint8_t lost = 0;
+	if(mydata->new_message == 0){
+		lost = 1;
+	}
+	return lost;
+}
+
 
 void message_rx (message_t *message, distance_measurement_t *distance){
     mydata->new_message = 1;
-
 		mydata->toAggregate.dist = estimate_distance(distance);
 		printf("DISTANCE.toAggregate : %d\n",mydata->toAggregate.dist);
 }
 
-//message_t *message_tx(){
-//    return &message;
-//}
+message_t *message_tx(){
+	if(mydata->broadcast){
+		printf("BROADCASTING %d\n",mydata->message.data[0]);
+		//return &mydata->message;
+		printf("Adresse message : %d\n", &(mydata->message));
+		return &(mydata->message);
+	} else {
+  	return 0;
+	}
+
+	//return &(mydata->message);
+}
+
+void message_tx_success()
+{
+    // Set the flag on message transmission.
+    mydata->message_sent = 1;
+}
 
 
 int main() {
@@ -183,7 +242,10 @@ int main() {
 		debug_init();
 
     kilo_message_rx = message_rx;
-    //kilo_message_tx = message_tx;
+    kilo_message_tx = message_tx;
+		kilo_message_tx_success = message_tx_success;
+
+
     kilo_start(setup, aggregation);
 
     return 0;
--- aggregation_1_robot/aggregation.h
+++ aggregation_1_robot/aggregation.h
@@ -1,5 +1,5 @@
-#ifndef MAIN__H
-    #define MAIN__H
+#ifndef AGGREGATION__H
+    #define AGGREGATION__H
 
 
     void setup(void);
@@ -13,6 +13,7 @@
     void set_random_direction(void);
     uint8_t is_too_close(void);
     void set_random_turning_direction(void);
+    int lost_aggregate(void);
 
     typedef struct {
       uint8_t dist;                    /*!< the last known dist of the neighbor */
@@ -22,6 +23,7 @@
     typedef struct
     {
       uint8_t state;
+
     	uint8_t curr_motion;
     	uint32_t last_motion_update;
     	uint8_t new_message;
@@ -30,6 +32,10 @@
       uint8_t last_dist_update;
 
       Neighbor_t toAggregate;
+
+      message_t message;
+      uint8_t broadcast;
+      uint8_t message_sent;
 
     } data_t;
 
@@ -45,4 +51,5 @@
     CONVERGING,          /*!< The robot is moving to a neighbor */
     SLEEPING                /*!< The robot is sleeping */
     };
+
 #endif
