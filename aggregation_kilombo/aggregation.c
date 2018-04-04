c434db1a610ed0e833bc200e468c431afbb288ed
Julien Andres <julienandres06@gmail.com>
2018-04-05 00:06:18 +0200

    maj kilombo & start aggregation proba

--- aggregation_kilombo/aggregation.c
+++ aggregation_kilombo/aggregation.c
@@ -32,12 +32,12 @@ void emission(){
 	// Blink the LED magenta whenever a message is sent.
 	if (mydata->message_sent == 1)
 	{
-		printf("EMISSION %d\n",kilo_uid );
+		//printf("EMISSION %d\n",kilo_uid );
 		mydata->message_sent = 0;
-		set_color(RGB(1, 0, 1));
+		set_color(RGB(0, 0, 1));
 			if (kilo_ticks>mydata->last_dist_update+(SECONDE/5)){
 				mydata->last_dist_update=kilo_ticks;
-				set_color(RGB(0, 0, 0));
+
 			}
 	}
 }
@@ -52,6 +52,7 @@ void aggregation() {
 		mydata->last_motion_update = kilo_ticks;
 
 		switch(mydata->state){
+
 			case SEARCHING:
 				searching();
 				break;
@@ -76,7 +77,6 @@ void searching(){
 //	printf("NEW MESSAGE : %d\n", mydata->new_message);
 	if(found_to_aggregate()){
 		mydata->state = CONVERGING;
-		//mydata->curr_motion = STRAIGHT;
 		set_motion(STRAIGHT);
 		mydata->last_dist_update = mydata->toAggregate.dist;
 	} else {
@@ -96,21 +96,20 @@ uint8_t found_to_aggregate(){
 }
 
 void converging(){
+	// printf("MOTION : %d\n",mydata->curr_motion);
+
 	set_color(RGB(0,1,0));
 	if(is_too_close()){
 		mydata->state = SLEEPING;
-		//mydata->curr_motion = STOP;
 		set_motion(STOP);
 	} else {
-		if(mydata->toAggregate.dist < mydata->last_dist_update){
-			printf("IN TEST");
+		if(mydata->toAggregate.dist >= mydata->last_dist_update){
+			// printf("IN TEST\n");
 			switch (mydata->curr_motion) {
 				case RIGHT:
-					//mydata->curr_motion = LEFT;
 					set_motion(LEFT);
 					break;
 				case LEFT:
-					//mydata->curr_motion = RIGHT;
 					set_motion(RIGHT);
 					break;
 				case STRAIGHT:
@@ -129,7 +128,7 @@ void converging(){
 }
 
 void sleeping(){
-	//set_color(RGB(0,0,1));
+	set_color(RGB(0,0,1));
 	set_motion(STOP);
 	mydata->broadcast = 1;
 	//enable_tx = 1;
@@ -140,21 +139,12 @@ void sleeping(){
 		mydata->new_message = 0;
 	}
 
-	if (mydata->message_sent == 1)
-	{
-			// Reset the flag so the LED is only blinked once per message.
-			mydata->message_sent = 0;
-
-			set_color(RGB(1, 0, 1));
-			delay(100);
-			set_color(RGB(0, 0, 0));
-	}
-	printf("broadcast : %d\n", mydata->broadcast);
+	//printf("broadcast : %d\n", mydata->broadcast);
 }
 
 uint8_t is_too_close(){
 	uint8_t stop = 0;
-	printf("dist dans is_too_close %d\n", mydata->toAggregate.dist);
+	//printf("dist dans is_too_close %d\n", mydata->toAggregate.dist);
 	if(mydata->toAggregate.dist <= DIST_TO_AGGREGATE){
 
 		stop = 1;
@@ -191,26 +181,22 @@ void set_random_direction(){
     //Compute the remainder of random_number when divided by 3.
     // This gives a new random number in the set {0, 1, 2}. The value is incremented and returned.
     if ((random_number == 0) || (random_number == 3)){
-        //mydata->curr_motion = STRAIGHT;
 				set_motion(STRAIGHT);
     }
     else if(random_number == 1){
-        //mydata->curr_motion = LEFT;
 				set_motion(LEFT);
     }
     else{
-        //mydata->curr_motion = RIGHT;
 				set_motion(RIGHT);
     }
 }
 
 void set_random_turning_direction(){
+	//printf("ici N?\n" );
 	uint8_t random_number = rand_hard()%2;
 	if(random_number == 0){
-		//mydata->curr_motion = LEFT;
 		set_motion(LEFT);
 	} else {
-		//mydata->curr_motion = RIGHT;
 		set_motion(RIGHT);
 	}
 }
@@ -227,7 +213,7 @@ int lost_aggregate(){
 void message_rx (message_t *message, distance_measurement_t *distance){
     mydata->new_message = 1;
 		mydata->toAggregate.dist = estimate_distance(distance);
-		printf("DISTANCE.toAggregate : %d\n",mydata->toAggregate.dist);
+		//if (kilo_uid==8) printf("DISTANCE.toAggregate : %d\n",mydata->toAggregate.dist);
 }
 
 message_t *message_tx(){
@@ -235,9 +221,9 @@ message_t *message_tx(){
 		return &mydata->message;
 	}
 	if(mydata->broadcast){
-		printf("BROADCASTING %d\n",mydata->message.data[0]);
+		//printf("BROADCASTING %d\n",mydata->message.data[0]);
 		//return &mydata->message;
-		printf("Adresse message : %d\n", &(mydata->message));
+		//printf("Adresse message : %d\n", &(mydata->message));
 		return &(mydata->message);
 	} else {
   	return 0;
---
+++ aggregation_proba_kilombo/test.h
@@ -0,0 +1,38 @@
+
+#define MAXVOISIN 5 //nombre de voisins dans la liste
+#define SECONDE 32 //nombre de tick en 1 seconde
+#define ASK 70  //mydata->distance minimale entre 2 kilobots
+
+#define STOP 0
+#define STRAIGHT 1
+#define LEFT 2
+#define RIGHT 3
+
+
+
+// declare variables
+
+
+typedef struct{   //Sert à garder les parametres de chacun des voisins
+  uint32_t timestamp; //timestamp de la recetion du message de ce voisin
+  uint8_t dist;  //mydata->distance estimé de ce voisin au dernier timestamp
+  uint16_t id;  //id de ce voisin
+  uint16_t nb_voisins;//nb de voisins de ce voisin
+}voisins;
+
+typedef struct{
+  uint8_t nb_voisins;  //nombre de voisins dans la liste
+  voisins voisins_liste[MAXVOISIN]; //liste des voisins
+
+  uint8_t next_direction;
+  uint8_t previous_dir;
+  int last_update; //derniere update de la direction
+
+
+  message_t messagetx;    //message transmis
+  message_t messagerx;    //message recu
+  uint8_t new_message;   //est mis à 1 si un message arrive
+  uint8_t distance;    //mydata->distance estimé du kilobot qui evoir le message
+
+  uint8_t nb;
+} USERDATA;
